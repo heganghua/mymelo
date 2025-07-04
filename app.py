f97c8e8334987888dfa023757cf2784c8369031f
Ganghua <1315579747@qq.com>
2025-07-04 17:12:05 +0800

    add app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
+@app.route('/works')
+def works():
+    return render_template('works.html')
+
+if __name__ == '__main__':
+    app.run(debug=True) 
